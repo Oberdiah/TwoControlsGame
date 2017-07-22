7cc991608ae436aa6d904b71ae0a32c2111368bb
Project <unconfigured@null.spigotmc.org>
2017-07-22 14:33:01 +0100

    Fixed Merge Disaster

--- Two Control Game/src/mainstuff/Main.java
+++ Two Control Game/src/mainstuff/Main.java
@@ -52,7 +52,6 @@ public class Main extends JPanel{
 		frame.setSize(1000, 1000);
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-<<<<<<< HEAD
 		
 		long lastLoopTime = System.nanoTime();
         final int TARGET_FPS = 100;
@@ -76,10 +75,7 @@ public class Main extends JPanel{
            long updateLength = now - lastLoopTime;
            lastLoopTime = now;
            double delta = updateLength / ((double)OPTIMAL_TIME);
-=======
->>>>>>> branch 'master' of https://github.com/Oberdiah/TwoControlsGame.git
-
-<<<<<<< HEAD
+           
            // update the frame counter
            lastFpsTime += updateLength;
            fps++;
@@ -110,52 +106,4 @@ public class Main extends JPanel{
     	   }
         }
     }
-=======
-		long lastLoopTime = System.nanoTime();
-		final int TARGET_FPS = 100;
-		final long OPTIMAL_TIME = 1000000000 / TARGET_FPS;
-		long lastFpsTime = 0;
-		long fps = 0;
-
-		while (true) {
-			// work out how long its been since the last update, this
-			// will be used to calculate how far the entities should
-			// move this loop
-			long now = System.nanoTime();
-			long updateLength = now - lastLoopTime;
-			lastLoopTime = now;
-			double delta = updateLength / ((double)OPTIMAL_TIME);
-
-			// update the frame counter
-			lastFpsTime += updateLength;
-			fps++;
-
-			// update our FPS counter if a second has passed since
-			// we last recorded
-			if (lastFpsTime >= 1000000000)
-			{
-				System.out.println("(FPS: "+fps+")");
-				lastFpsTime = 0;
-				fps = 0;
-			}
-
-			// draw everyting
-			game.repaint();
-
-			// update the game logic
-			MainTick.goUpdates(delta);
-
-			// we want each frame to take 10 milliseconds, to do this
-			// we've recorded when we started the frame. We add 10 milliseconds
-			// to this and then factor in the current time to give 
-			// us our final value to wait for
-			// remember this is in ms, whereas our lastLoopTime etc. vars are in ns.
-			try{
-				Thread.sleep( (lastLoopTime-System.nanoTime() + OPTIMAL_TIME)/1000000);
-			} catch(Exception e) {
-
-			}
-		}
-	}
->>>>>>> branch 'master' of https://github.com/Oberdiah/TwoControlsGame.git
 }
