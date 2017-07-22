4de503d584800215e6fcc60ab7fdc1c1e38a4909
Oberdiah <Oberdiah@gmail.com>
2017-07-22 15:48:42 +0100

    Changed something about Main.java. Not sure what it is.

--- Two Control Game/src/mainstuff/Main.java
+++ Two Control Game/src/mainstuff/Main.java
@@ -30,8 +30,6 @@ public class Main extends JPanel{
 	@Override
 	public void paint(Graphics g) {
 		GGeneral.gogo((Graphics2D) g);
-
-
 	}
 
 	public static void main(String[] args) throws InterruptedException  {
